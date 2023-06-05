c9babfde0d8818619dafeb6dc5a0b938d7ee1ef4
Freeze35 <HumanSolidarity@protonmail.com>
2023-06-06 00:35:41 +0800

    v1.0.1 Virtual-Scroll modify (offset,zIndex,opacity)

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -7,6 +7,7 @@ interface SliderInterface {
 interface colorsInterface {
     id: number;
     localImg: string;
+    leftOffset?:number;
 }
 
 const Slider: React.FC<SliderInterface> = ({style}) => {
@@ -28,18 +29,35 @@ const Slider: React.FC<SliderInterface> = ({style}) => {
     useEffect(()=>{
         setTimeout(()=>{
             setPositionImage(prev=>prev+1)
-            if(positionImage === images.length-1){
+            /*if(positionImage === images.length-1){
                 setPositionImage(0)
-            }
+            }*/
         },5000)
     },[positionImage])
 
+    const zIndexArea = (offset:number|undefined) =>{
+        console.log(positionImage * 100 , offset)
+       return  positionImage * 100 === offset
+            ? 5
+            : 4
+    }
+    const opacityView = (offset:number|undefined) =>{
+        console.log(positionImage * 100 , offset)
+
+        return  positionImage * 100 === offset || positionImage === 0
+            ? 1
+            : 0
+
+    }
 
     return (
         <div style={{...style}} className="slider_block animate">
             {images.map((image, index) =>
 
                 <div key={index} className={"one_slide"} style={{
+                    opacity: opacityView(image.leftOffset),
+                    zIndex: zIndexArea(image.leftOffset) ,
+                    marginLeft:`${image.leftOffset}%`,
                     backgroundImage: `url(${image.localImg})`,
                     backgroundSize:"100% 100%",
                     transform: `translateX(-${positionImage * 100}%)`,
@@ -51,107 +69,45 @@ const Slider: React.FC<SliderInterface> = ({style}) => {
 
 export default Slider;
 
-//tests materials
 
-//const [changelist, setChangeList] = useState<colorsInterface[]>([])
+//const [changeList, setChangeList] = useState<colorsInterface[]>([])
 
 /*useLayoutEffect(() => {
-    setChangeList([...color.slice(0, 3).map((col,id) => {
 
-        col.leftOffset = (positionColor+id) * 100
-        return col
-    })])
-},[])
-
-useLayoutEffect(() => {
-    if ((positionColor+1) % 3 === 0  ) {
-
-        const checkPosition=()=>{
-
-                if(color.length- positionColor+3)
-            {
-                return positionColor+3
+        const checkFirstPosition = () =>{
+            if(positionImage + 1 % images.length === 0 || positionImage === 0){
+                return 0
             }
             else {
-                return positionColor+color.length-positionColor
+                return positionImage % images.length
             }
         }
-        setChangeList([...color.slice(positionColor-2, checkPosition()).map((col,id) => {
 
-            col.leftOffset = (positionColor+id-2) * 100
-            return col
-        })])}
-}, [positionColor])*/
+        const checkLastPosition=()=>{
 
-
-/* console.log([...color.slice(0, 3).map(col => {
-     col.leftOffset = posOffset
-     return col
- })])*/
-/*else if((positionColor+1) % color.length ===0) {
-    setChangeList([...color.slice(0, 3).map((col,id) => {
-
-        col.leftOffset = (positionColor+id-1) * 100
-        return col
-    })])}*/
-/*/!*setChangeList([...color.slice(0, 3).map((col,id) => {
-
-    col.leftOffset = (positionColor+id-1) * 100
-    return col
-})])}*!/
-let prevElem =color[color.length-2]
-prevElem = {...prevElem,leftOffset:(positionColor-1) * 100}
-let lastElem =color[color.length-1]
-lastElem = {...lastElem,leftOffset:(positionColor) * 100}
-console.log(changelist)
-setChangeList([...changelist,
-    ...color.slice(0, 3).map((col,id) => {
-
-    col.leftOffset = (positionColor+id-1) * 100
-    return col
-})])*/
-
-
-/*console.log([changelist[changelist.length-1],...color.slice(0, 2).map((col,id) => {
-
-    col.leftOffset = changelist[changelist.length-1].leftOffset+(positionColor+id-2) * 100
-    return col
-})])}*/
-/*setChangeList([changelist[changelist.length-1],...color.slice(0, 2).map((col,id) => {
-
-    col.leftOffset = changelist[changelist.length-1].leftOffset+(positionColor+id-2) * 100
-    return col
-})])
-}
-/*else if((positionColor+1) % color.length ===0) {
-
-
-console.log([...color.slice(0, 2).map((col,id) => {
-
-    col.leftOffset = (positionColor+id+2) * 100
-    return col
-})])
-let newList = [...color.slice(0, 2).map((col,id) => {
-
-    col.leftOffset = (positionColor+id+2) * 100
-    return col
-})]
-setChangeList(newList)
-
-}*/
-
-/*if (positionColor > color.length - 1) {
-    setChangeList([...changelist.map(item=> {
-            item = {...item, leftOffset: (positionColor) * 100}
-            return item
+            if(positionImage+3 % images.length>0)
+            {
+                return positionImage % images.length+3
+            }
         }
-    )])
-}*/
+    console.log(checkFirstPosition(), checkLastPosition())
+        const getNewSlice = () =>{
+           return [...images.slice(checkFirstPosition(), checkLastPosition()).map(
+                (image:colorsInterface,id) => {
+
+                    image.leftOffset = (positionImage+id+1) * 100
+                    return image
+                })]
+        }
 
 
+    if((positionImage + 1) % images.length === 0){
+        setChangeList([...images.slice(-1),
+            ...getNewSlice()])
+    }
+    else {
+        setChangeList([...getNewSlice()])
+    }
 
-/*useEffect(() => {
-    setTimeout(() => {
-        setPositionColor(prev=>prev+1)
-    }, 1000)
-}, [positionColor])*/
+
+}, [positionImage])*/
